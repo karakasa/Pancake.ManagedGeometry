c0823a206f428d30d0bc88d08edf7570e759e0fd
Gan Keyu <gankeyu@hotmail.com>
2022-07-20 18:03:58 +0800

    added functions to fastio

--- Pancake.ModernUtility/FastIo.cs
+++ Pancake.ModernUtility/FastIo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Runtime.CompilerServices;
 
 namespace Pancake.ModernUtility
 {
@@ -14,6 +15,54 @@ namespace Pancake.ModernUtility
             {
                 var span = new ReadOnlySpan<byte>(ptr, sizeof(T) * array.Length);
                 stream.Write(span);
+            }
+        }
+
+        public static void Read<T>(this Stream stream, T[] array)
+            where T : unmanaged
+        {
+            if (array.Length == 0) return;
+
+            fixed (T* ptr = &array[0])
+            {
+                var span = new Span<byte>(ptr, sizeof(T) * array.Length);
+                stream.Read(span);
+            }
+        }
+        public static int ReinterpretCopy<TSource, TDestination>(TSource[] source, TDestination[] destination)
+            where TSource : unmanaged
+            where TDestination : unmanaged
+        {
+            if (sizeof(TSource) != sizeof(TDestination)) throw new InvalidOperationException("Sizes mismatch.");
+
+            if (source.Length == 0) return 0;
+
+            fixed (TSource* ptrSource = &source[0])
+            fixed (TDestination* ptrDest = &destination[0])
+            {
+                var actualCopyLength = (source.Length < destination.Length) ? source.Length : destination.Length;
+                var spanSource = new ReadOnlySpan<byte>(ptrSource, sizeof(TSource) * actualCopyLength);
+                var spanTarget = new Span<byte>(ptrDest, sizeof(TDestination) * actualCopyLength);
+
+                spanSource.CopyTo(spanTarget);
+
+                return actualCopyLength;
+            }
+        }
+
+        // TODO: Isn't this action illegal? Needs to be paired with No-GC region.
+        public static ReadOnlySpan<TDestination> Reinterpret<TSource, TDestination>(TSource[] source)
+            where TSource : unmanaged
+            where TDestination : unmanaged
+        {
+            if (sizeof(TSource) != sizeof(TDestination)) throw new InvalidOperationException("Sizes mismatch.");
+
+            if (source.Length == 0) return Array.Empty<TDestination>();
+
+            fixed (TSource* ptrSource = &source[0])
+            {
+                var spanSource = new ReadOnlySpan<TDestination>(ptrSource, source.Length);
+                return spanSource;
             }
         }
     }
